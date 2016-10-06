157223b929aa0f9cf72c75560bd9005e3887c47a
Caio Cutrim <kaiocoutrim@gmail.com>
2016-10-06 09:42:46 -0300

    init more helpers

--- client/startup.js
+++ client/startup.js
@@ -5,9 +5,10 @@ import '../imports/configuration/Routers';
 
 /**helpers */
 import '../imports/ui/auth_helper.js';
-import '../imports/ui/home_helper.js';
+import '../imports/ui/home_helpers.js';
 import '../imports/ui/private_post_helper.js';
 import '../imports/ui/public_post_helper.js';
 import '../imports/ui/posts_helper.js';
 import '../imports/ui/post_insert_form_helper.js';
-import '../imports/ui/mainLayout_helper';
+import '../imports/ui/mainLayout_helper.js';
+import '../imports/ui/profile_helper.js';
