0e5d920c7d445eda2b365e0082a403dd1e4eb179
Caio Cutrim <kaiocoutrim@gmail.com>
2016-10-06 12:23:45 -0300

    more clean code

--- imports/ui/posts_helper.js
+++ imports/ui/posts_helper.js
@@ -6,7 +6,6 @@ import { sAlert } from 'meteor/juliancwirko:s-alert';
 /*
  * ui dependencies
  */
-import './post_insert_form.html';
 import './message.html';
 import './posts.html';
 import './post_list.html';
@@ -18,26 +17,5 @@ Template.posts.events({
   },
  'click .cancel'() {
     $('#insert-form').css({'display':'none'});
-  }
-});
-
-/**
- * insert helper
- */
-Template.post_insert_form.events({
-  'submit #insert-form'(event) {
-    event.preventDefault();
-    let target = event.target;
-    let title = target.title.value;
-    let url = target.url.value;
-    Meteor.call('posts.insertPost', title, url, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        sAlert.info('You did a post');
-        target.title.value = '';
-        target.url.value = '';
-      }
-    });
   }
 });
