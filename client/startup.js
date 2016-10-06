48f1430112dd2ffed903ffdf21dfca5544ede1f6
Caio Cutrim <kaiocoutrim@gmail.com>
2016-10-06 12:21:34 -0300

    import helpers

--- client/startup.js
+++ client/startup.js
@@ -4,6 +4,9 @@ import '../imports/configuration/Routers';
 
 
 /**helpers */
+
+import '../imports/ui/comments_helpers.js';
+import '../imports/ui/post_insert_form_helper.js';
 import '../imports/ui/auth_helper.js';
 import '../imports/ui/home_helpers.js';
 import '../imports/ui/private_post_helper.js';
@@ -12,3 +15,4 @@ import '../imports/ui/posts_helper.js';
 import '../imports/ui/post_insert_form_helper.js';
 import '../imports/ui/mainLayout_helper.js';
 import '../imports/ui/profile_helper.js';
+import '../imports/ui/best_posts_helper.js';
